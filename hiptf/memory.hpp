45da921ca81509a38f6389e119b51e725c474d6a
mutsuki <mutsuki@momo86.net>
2020-05-22 09:59:35 +0900

    Create memory.hpp

---
+++ hiptf/memory.hpp
@@ -0,0 +1,11 @@
+#ifndef __HIPTF_MEMORY_HPP__
+#define __HIPTF_MEMORY_HPP__
+#include <memory>
+
+namespace hiptf {
+namespace memory {
+
+} // namespace memory
+} // namespace hiptf
+
+#endif /* end of include guard */
