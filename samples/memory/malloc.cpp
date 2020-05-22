545eba3f5bc6bd507999c36fb8793af250ea9c49
mutsuki <mutsuki@momo86.net>
2020-05-22 13:25:13 +0900

    Remove CUDA code

--- samples/memory/malloc.cpp
+++ samples/memory/malloc.cpp
@@ -4,16 +4,6 @@
 constexpr std::size_t N = 1 << 17;
 constexpr std::size_t threads_per_block = 1 << 8;
 
-template <class T, std::size_t N>
-__global__ void pow2_kernel(T* const C){
-	const auto tid = blockIdx.x * blockDim.x + threadIdx.x;
-	if(tid >= N) return;
-
-	const auto c = __ldg(C + tid);
-
-	C[tid] = c * c;
-}
-
 using compute_t = float;
 int main(){
 	auto dA = hiptf::memory::get_device_unique_ptr<compute_t>(N);
@@ -29,13 +19,4 @@ int main(){
 		hA.get()[i] = hiptf::type::cast<compute_t>(1.0f * i / N);
 		hB.get()[i] = hiptf::type::cast<compute_t>(1.0f * (N - i) / N);
 	}
-
-	hiptf::memory::copy_async(dA.get(), hA.get(), N, *streamA.get());
-	hiptf::memory::copy_async(dB.get(), hB.get(), N, *streamB.get());
-
-	pow2_kernel<compute_t, N><<<(N + threads_per_block - 1) / threads_per_block, threads_per_block, 0, *streamA.get()>>>(dA.get());
-	pow2_kernel<compute_t, N><<<(N + threads_per_block - 1) / threads_per_block, threads_per_block, 0, *streamB.get()>>>(dB.get());
-
-	hiptf::memory::copy_async(dA.get(), hA.get(), N, *streamA.get());
-	hiptf::memory::copy_async(dB.get(), hB.get(), N, *streamB.get());
 }
