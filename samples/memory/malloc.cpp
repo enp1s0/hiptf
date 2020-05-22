2a048262be80fc5f476638c7fb02d7e126330627
mutsuki <mutsuki@momo86.net>
2020-05-22 14:14:10 +0900

    Remove `type` code

--- samples/memory/malloc.cpp
+++ samples/memory/malloc.cpp
@@ -10,13 +10,4 @@ int main(){
 	auto dB = hiptf::memory::get_device_unique_ptr<compute_t>(N);
 	auto hA = hiptf::memory::get_host_unique_ptr<compute_t>(N);
 	auto hB = hiptf::memory::get_host_unique_ptr<compute_t>(N);
-
-	auto streamA = hiptf::stream::get_stream_unique_ptr();
-	auto streamB = hiptf::stream::get_stream_unique_ptr();
-
-
-	for(auto i = decltype(N)(0); i < N; i++){
-		hA.get()[i] = hiptf::type::cast<compute_t>(1.0f * i / N);
-		hB.get()[i] = hiptf::type::cast<compute_t>(1.0f * (N - i) / N);
-	}
 }
