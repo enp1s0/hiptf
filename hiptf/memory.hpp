251d03a6bab37ef04b2a2be92d1199020efcdda0
mutsuki <mutsuki@momo86.net>
2020-05-22 10:40:33 +0900

    Add deleters and unique pointers

--- hiptf/memory.hpp
+++ hiptf/memory.hpp
@@ -4,6 +4,27 @@
 
 namespace hiptf {
 namespace memory {
+// deleter
+template <class T>
+class device_deleter {
+public:
+	void operator()(T* ptr){
+		hipFree(ptr);
+	}
+};
+template <class T>
+class host_deleter {
+public:
+	void operator()(T* ptr){
+		delete [] ptr;
+	}
+};
+
+// unique pointer type for c++ 11
+template <class T>
+using device_unique_ptr = std::unique_ptr<T, device_deleter<T>>;
+template <class T>
+using host_unique_ptr = std::unique_ptr<T, host_deleter<T>>;
 
 } // namespace memory
 } // namespace hiptf
