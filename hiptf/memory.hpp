58195cac6bf751f1dd79e71d363f673ffdec3631
mutsuki <mutsuki@momo86.net>
2020-06-01 21:28:17 +0900

    Update to use `HIPTF_CHECK_ERROR`

--- hiptf/memory.hpp
+++ hiptf/memory.hpp
@@ -11,14 +11,14 @@ template <class T>
 class device_deleter {
 public:
 	void operator()(T* ptr){
-		HIPTF_HANDLE_ERROR(hipFree(ptr));
+		HIPTF_CHECK_ERROR(hipFree(ptr));
 	}
 };
 template <class T>
 class host_deleter {
 public:
 	void operator()(T* ptr){
-		HIPTF_HANDLE_ERROR(hipHostFree(ptr));
+		HIPTF_CHECK_ERROR(hipHostFree(ptr));
 	}
 };
 
@@ -32,13 +32,13 @@ using host_unique_ptr = std::unique_ptr<T, host_deleter<T>>;
 template <class T>
 inline device_unique_ptr<T> get_device_unique_ptr(const std::size_t size){
 	T* ptr;
-	HIPTF_HANDLE_ERROR_M(hipMalloc((void**)&ptr, sizeof(T) * size), "Failed to allocate " + std::to_string(size * sizeof(T)) + " Bytes of device memory");
+	HIPTF_CHECK_ERROR_M(hipMalloc((void**)&ptr, sizeof(T) * size), "Failed to allocate " + std::to_string(size * sizeof(T)) + " Bytes of device memory");
 	return std::unique_ptr<T, device_deleter<T>>{ptr};
 }
 template <class T>
 inline host_unique_ptr<T> get_host_unique_ptr(const std::size_t size){
 	T* ptr;
-	HIPTF_HANDLE_ERROR_M(hipHostMalloc((void**)&ptr, sizeof(T) * size), "Failed to allocate " + std::to_string(size * sizeof(T)) + " Bytes of host memory");
+	HIPTF_CHECK_ERROR_M(hipHostMalloc((void**)&ptr, sizeof(T) * size), "Failed to allocate " + std::to_string(size * sizeof(T)) + " Bytes of host memory");
 	return std::unique_ptr<T, host_deleter<T>>{ptr};
 }
 
