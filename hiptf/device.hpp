7f7416f9f798ebe79fe0ff155a56d8841308957d
mutsuki <mutsuki@momo86.net>
2020-06-07 11:40:08 +0900

    Add `use_device`

--- hiptf/device.hpp
+++ hiptf/device.hpp
@@ -1,6 +1,7 @@
 #ifndef __HIPTF_DEVICE_HPP__
 #define __HIPTF_DEVICE_HPP__
 #include <cstddef>
+#include <functional>
 #include <hip/hip_runtime.h>
 
 namespace hiptf {
@@ -9,6 +10,20 @@ inline std::size_t get_num_devices() {
 	int n;
 	hipGetDeviceCount(&n);
 	return static_cast<std::size_t>(n);
+}
+
+inline hipError_t use_device(const int device_id, const std::function<void(void)> function){
+	int current_device_id;
+	hipError_t result;
+	result = hipGetDevice(&current_device_id);
+	if(result != hipSuccess) {return result;}
+	result = hipSetDevice(device_id);
+	if(result != hipSuccess) {return result;}
+	function();
+	result = hipSetDevice(current_device_id);
+	if(result != hipSuccess) {return result;}
+
+	return hipSuccess;
 }
 } // namespace device
 } // namespace hiptf
