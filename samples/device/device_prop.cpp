423ed82e2c4e955d7f4efc676854de85c2fac203
mutsuki <mutsuki@momo86.net>
2020-06-07 11:53:04 +0900

    Add the test of `hiptf::device::get_num_devices`

--- samples/device/device_prop.cpp
+++ samples/device/device_prop.cpp
@@ -4,6 +4,8 @@
 int main(){
 	const auto device_props = hiptf::device::get_properties_vector();
 
+	std::printf("# num devices = %lu\n", hiptf::device::get_num_devices());
+
 	std::printf("# device names\n");
 	for (const auto device_prop : device_props) {
 		std::printf("%s\n", device_prop.name);
