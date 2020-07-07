6a13e4cd08b589629862f9871f095ee890648841
mutsuki <mutsuki@momo86.net>
2020-07-07 23:56:16 +0900

    Add math functions for floating point values

--- hiptf/math.hpp
+++ hiptf/math.hpp
@@ -31,6 +31,62 @@ template<>  __device__ inline double func<double>(const double a, const double b
 namespace hiptf {
 namespace math {
 
+MATH_FUNC_1(acos);
+MATH_FUNC_1(acosh);
+MATH_FUNC_1(asin);
+MATH_FUNC_1(asinh);
+MATH_FUNC_1(atan);
+MATH_FUNC_1(atanh);
+MATH_FUNC_1(ceil);
+MATH_FUNC_1(cbrt);
+MATH_FUNC_1(cos);
+MATH_FUNC_1(cosh);
+MATH_FUNC_1(erfc);
+MATH_FUNC_1(erf);
+MATH_FUNC_1(exp10);
+MATH_FUNC_1(exp2);
+MATH_FUNC_1(exp);
+MATH_FUNC_1(expm1);
+MATH_FUNC_1(fabs);
+MATH_FUNC_1(floor);
+MATH_FUNC_1(log10);
+MATH_FUNC_1(log1p);
+MATH_FUNC_1(logb);
+MATH_FUNC_1(log2);
+MATH_FUNC_1(log);
+MATH_FUNC_1(round);
+MATH_FUNC_1(sin);
+MATH_FUNC_1(sinh);
+MATH_FUNC_1(sqrt);
+MATH_FUNC_1(tan);
+MATH_FUNC_1(tanh);
+MATH_FUNC_1(trunc);
+MATH_FUNC_1(erfcinv);
+MATH_FUNC_1(erfcx);
+MATH_FUNC_1(erfinv);
+MATH_FUNC_1(j0);
+MATH_FUNC_1(j1);
+MATH_FUNC_1(lgamma);
+MATH_FUNC_1(nextafter);
+MATH_FUNC_1(normcdf);
+MATH_FUNC_1(normcdfinv);
+MATH_FUNC_1(rcbrt);
+MATH_FUNC_1(rint);
+MATH_FUNC_1(y0);
+MATH_FUNC_1(y1);
+
+// 2-
+MATH_FUNC_2(atan2);
+MATH_FUNC_2(copysign);
+MATH_FUNC_2(fdim);
+MATH_FUNC_2(fmax);
+MATH_FUNC_2(fmin);
+MATH_FUNC_2(fmod);
+MATH_FUNC_2(hypot);
+MATH_FUNC_2(pow);
+MATH_FUNC_2(fdivide);
+MATH_FUNC_2(rhypot);
+
 } // namespace math
 } // namespace hiptf
 
