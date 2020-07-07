4c8bf94c208f292e7fceab48473c79d6c0d928ea
mutsuki <mutsuki@momo86.net>
2020-07-07 23:40:59 +0900

    Add `SPEC_MATH_FUNC` macros

--- hiptf/math.hpp
+++ hiptf/math.hpp
@@ -2,7 +2,23 @@
 #define __HIPTF_MATH_HPP__
 #include <hip/hip_runtime.h>
 
+#define DEF_TEMPLATE_MATH_FUNC_1(func) \
+template<class T>  __device__ inline T func(const T a);
+
+#define SPEC_MATH_FUNC_1_f( func ) \
+template<>  __device__ inline float func<float>(const float a){return func##f( a );}
+#define SPEC_MATH_FUNC_1_d( func ) \
+template<>  __device__ inline double func<double>(const double a){return func( a );}
+#define MATH_FUNC(func) \
+	DEF_TEMPLATE_MATH_FUNC_1(func) \
+	SPEC_MATH_FUNC_1_f(func) \
+	SPEC_MATH_FUNC_1_d(func) \
+
+
 namespace hiptf {
+namespace math {
+
+} // namespace math
 } // namespace hiptf
 
 #endif /* end of include guard */
