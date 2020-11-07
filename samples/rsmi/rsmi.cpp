90d70a8e7aeb5da8910863125f3eaa6495030a2f
mutsuki <mutsuki@momo86.net>
2020-11-07 14:31:32 +0900

    Update the test for rsmi

--- samples/rsmi/rsmi.cpp
+++ samples/rsmi/rsmi.cpp
@@ -3,4 +3,8 @@
 
 int main(){
 	HIPTF_CHECK_ERROR(rsmi_init(0));
+	int64_t temperature;
+	HIPTF_CHECK_ERROR(rsmi_dev_temp_metric_get(0, 0, RSMI_TEMP_CURRENT, &temperature));
+
+	std::printf("temperature = %.1f\n", temperature / 1000.0);
 }
